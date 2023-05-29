21b996f43b7aa2ce4b5bab9d1fc1502197506dec
DefaultV <dozermailjd@gmail.com>
2023-05-29 16:21:55 +0200

    Update

--- index.js
+++ index.js
@@ -1,19 +1,34 @@
-var productItemConsulting = document.getElementById("product_consulting");
-var productItemDevelopment = document.getElementById("product_development");
-var productItemViewer = document.getElementById("product_viewer");
+var productItemContact = document.getElementsByClassName("contact");
+var productItemExpandable = document.getElementsByClassName("expandable");
+var lightbox = document.getElementsByClassName("lightbox")[0];
+var lightboxTitle = document.getElementsByClassName("lightbox-title")[0];
+var lightboxText = document.getElementsByClassName("lightbox-text")[0];
+var lightboxImage = document.getElementsByClassName("lightbox-image")[0];
 // Entry
 (function () {
-    console.log("Added");
-    productItemConsulting === null || productItemConsulting === void 0 ? void 0 : productItemConsulting.addEventListener("click", function () {
-        document.location.href =
-            "mailto:dozermailjd@gmail.com?subject=Consulting - From Drews&body=Hello From Drews! I'm interested in a meeting with you.";
+    // Don't ever do this
+    lightbox.children[0].addEventListener("click", function (ev) {
+        ev.stopPropagation();
     });
-    productItemDevelopment === null || productItemDevelopment === void 0 ? void 0 : productItemDevelopment.addEventListener("click", function () {
-        document.location.href =
-            "mailto:dozermailjd@gmail.com?subject=Development - From Drews&body=Hello From Drews! I'm interested in a meeting with you.";
+    lightbox.addEventListener("click", function (ev) {
+        lightbox.classList.remove("active");
     });
-    productItemViewer === null || productItemViewer === void 0 ? void 0 : productItemViewer.addEventListener("click", function () {
-        document.location.href =
-            "mailto:dozermailjd@gmail.com?subject=3D Viewer - From Drews&body=Hello From Drews! I'm interested in a meeting with you.";
-    });
+    for (var i = 0; i < productItemExpandable.length; i++) {
+        var expandableItem = productItemExpandable[i];
+        expandableItem === null || expandableItem === void 0 ? void 0 : expandableItem.addEventListener("click", function (ev) {
+            var _a;
+            var target = ev.target;
+            lightbox.classList.add("active");
+            lightboxTitle.textContent = target.children[0].textContent;
+            lightboxText.textContent = target.children[1].textContent;
+            lightboxImage.src = ((_a = target.parentElement) === null || _a === void 0 ? void 0 : _a.children[1]).src;
+        });
+    }
+    for (var i = 0; i < productItemContact.length; i++) {
+        var contactItem = productItemContact[i];
+        contactItem === null || contactItem === void 0 ? void 0 : contactItem.addEventListener("click", function () {
+            document.location.href =
+                "mailto:dozermailjd@gmail.com?subject=First Contact - Megabyte Magicians&body=Hej Megabyte Magicians! Jeg er interesseret i et møde med jer, hvordan passer det jer ...";
+        });
+    }
 })();
--- index.ts
+++ index.ts
@@ -1,22 +1,51 @@
-const productItemConsulting = document.getElementById("product_consulting");
-const productItemDevelopment = document.getElementById("product_development");
-const productItemViewer = document.getElementById("product_viewer");
+const productItemContact = document.getElementsByClassName("contact");
+const productItemExpandable = document.getElementsByClassName("expandable");
+const lightbox = document.getElementsByClassName(
+  "lightbox"
+)[0] as HTMLDivElement;
+const lightboxTitle = document.getElementsByClassName(
+  "lightbox-title"
+)[0] as HTMLDivElement;
+const lightboxText = document.getElementsByClassName(
+  "lightbox-text"
+)[0] as HTMLDivElement;
+const lightboxImage = document.getElementsByClassName(
+  "lightbox-image"
+)[0] as HTMLImageElement;
 
 // Entry
 (() => {
-  console.log("Added");
-  productItemConsulting?.addEventListener("click", () => {
-    document.location.href =
-      "mailto:dozermailjd@gmail.com?subject=Consulting - From Drews&body=Hello From Drews! I'm interested in a meeting with you.";
+  // Don't ever do this
+  lightbox.children[0].addEventListener("click", (ev) => {
+    ev.stopPropagation();
+  });
+  lightbox.addEventListener("click", (ev) => {
+    lightbox.classList.remove("active");
   });
 
-  productItemDevelopment?.addEventListener("click", () => {
-    document.location.href =
-      "mailto:dozermailjd@gmail.com?subject=Development - From Drews&body=Hello From Drews! I'm interested in a meeting with you.";
-  });
+  for (let i = 0; i < productItemExpandable.length; i++) {
+    const expandableItem = productItemExpandable[i];
 
-  productItemViewer?.addEventListener("click", () => {
-    document.location.href =
-      "mailto:dozermailjd@gmail.com?subject=3D Viewer - From Drews&body=Hello From Drews! I'm interested in a meeting with you.";
-  });
+    expandableItem?.addEventListener("click", (ev) => {
+      const target = ev.target as HTMLDivElement;
+
+      lightbox.classList.add("active");
+      lightboxTitle.textContent = target.children[0].textContent;
+      lightboxText.textContent = target.children[1].textContent;
+
+      // Bad practise
+      lightboxImage.src = (
+        target.parentElement?.children[1] as HTMLImageElement
+      ).src;
+    });
+  }
+
+  for (let i = 0; i < productItemContact.length; i++) {
+    const contactItem = productItemContact[i];
+
+    contactItem?.addEventListener("click", () => {
+      document.location.href =
+        "mailto:dozermailjd@gmail.com?subject=First Contact - Megabyte Magicians&body=Hej Megabyte Magicians! Jeg er interesseret i et møde med jer, hvordan passer det jer ...";
+    });
+  }
 })();
